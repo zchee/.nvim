8ac1bfe380381455c5a5f9278f06f3ae44b1b6df
Koichi Shiraishi <zchee.io@gmail.com>
2017-01-05 15:59:36 +0900

    template: update template
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- template/go/base-main.go
+++ template/go/base-main.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Koichi Shiraishi. All rights reserved.
+// Copyright {{_expr_:strftime('%Y')}} Koichi Shiraishi. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 

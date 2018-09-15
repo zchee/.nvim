8f9d928a6ad471b6bc4897a1e6a20f643da906f0
Koichi Shiraishi <zchee.io@gmail.com>
2018-09-15 13:52:40 +0900

    path/Frameworks: fix symlink.bash script

--- path/Frameworks/symlink.bash
+++ path/Frameworks/symlink.bash
@@ -8,8 +8,14 @@ if [[ -z $XCODE_PATH ]]; then
   exit 1
 fi
 
+DST_DIR="$(basename "${XCODE_PATH%.*}")"
+if [[ -d "$DST_DIR" ]]; then
+  rm -rf "$DST_DIR"
+fi
+mkdir -p "$DST_DIR"
+
 for d in $(find "$XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks" -maxdepth 1 -type d); do
   if [[ -d "$d/Headers" ]]; then
-    ln -s "$d/Headers" "$(printf $(basename $d) | awk -F. '{print $1}')"
+    command ln -fs "$d/Headers" "$DST_DIR/$(printf $(basename $d) | awk -F. '{print $1}')"
   fi
 done

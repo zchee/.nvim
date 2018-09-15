7c61b8e483b9dfb0fe90f17174e045be81a9a2a6
Koichi Shiraishi <zchee.io@gmail.com>
2018-09-15 13:58:57 +0900

    template: update to latest configs

--- template/go/base-main-owner.go
+++ template/go/base-main-owner.go
@@ -1,4 +1,4 @@
-// Copyright {{_expr_:strftime('%Y')}} The {{_expr_:fnamemodify(getcwd(), ':t')}} Authors. All rights reserved.
+// Copyright {{_expr_:strftime('%Y')}} Koichi Shiraishi. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 

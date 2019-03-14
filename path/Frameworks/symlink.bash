0b128d4b9a747b52de6572b262eddad763705155
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-14 14:17:22 +0900

    path/Frameworks: support nested frameworks

--- path/Frameworks/symlink.bash
+++ path/Frameworks/symlink.bash
@@ -1,5 +1,8 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -eo pipefail
+IFS=$'\n\t'
+
+set -x
 
 XCODE_PATH=$1
 
@@ -14,7 +17,34 @@ if [[ -d "$DST_DIR" ]]; then
 fi
 mkdir -p "$DST_DIR"
 
-for d in $(find "$XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks" -maxdepth 1 -type d); do
+for d in $(find "$XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks" -maxdepth 1 -type d -or -type l); do
+  if [[ -d "$d/Headers" ]]; then
+    command ln -fs "$d/Headers" "$DST_DIR/$(printf $(basename $d) | awk -F. '{print $1}')"
+  fi
+  if [[ -d "$d/ApplicationServices.framework/Versions/A/Frameworks" ]]; then
+    for dd in $(find "$d/ApplicationServices.framework/Versions/A/Frameworks" -maxdepth 1 -type d -or -type l); do
+      if [[ -d "$dd/Headers" ]]; then
+        command ln -fs "$dd/Headers" "$DST_DIR/$(printf $(basename $dd) | awk -F. '{print $1}')"
+      fi
+    done
+  fi
+  if [[ -d "$d/CoreServices.framework/Versions/A/Frameworks" ]]; then
+    for dd in $(find "$d/CoreServices.framework/Versions/A/Frameworks" -maxdepth 1 -type d -or -type l); do
+      if [[ -d "$dd/Headers" ]]; then
+        command ln -fs "$dd/Headers" "$DST_DIR/$(printf $(basename $dd) | awk -F. '{print $1}')"
+      fi
+    done
+  fi
+  if [[ -d "$d/Carbon.framework/Versions/A/Frameworks" ]]; then
+    for dd in $(find "$d/Carbon.framework/Versions/A/Frameworks" -maxdepth 1 -type d -or -type l); do
+      if [[ -d "$dd/Headers" ]]; then
+        command ln -fs "$dd/Headers" "$DST_DIR/$(printf $(basename $dd) | awk -F. '{print $1}')"
+      fi
+    done
+  fi
+done
+
+for d in $(find "$XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/PrivateFrameworks" -maxdepth 1 -type d); do
   if [[ -d "$d/Headers" ]]; then
     command ln -fs "$d/Headers" "$DST_DIR/$(printf $(basename $d) | awk -F. '{print $1}')"
   fi

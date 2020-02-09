f952545e6da6fa1c77c08fe9610ae6242ea1865c
Koichi Shiraishi <zchee.io@gmail.com>
2020-02-10 00:04:29 +0900

    path/Frameworks: fix symlink.bash script

--- path/Frameworks/symlink.bash
+++ path/Frameworks/symlink.bash
@@ -2,8 +2,6 @@
 set -eo pipefail
 IFS=$'\n\t'
 
-set -x
-
 XCODE_PATH=$1
 
 if [[ -z $XCODE_PATH ]]; then
@@ -17,38 +15,23 @@ if [[ -d "$DST_DIR" ]]; then
 fi
 mkdir -p "$DST_DIR"
 
-for d in $(dirname "$(find "$XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks" -maxdepth 1 -type d -or -type l)"); do
-  if [[ -d "$d/Headers" ]]; then
-    command ln -fs "$d/Headers" "$DST_DIR/$(printf $(basename $d) | awk -F. '{print $1}')"
-  fi
+find_framework_header() {
+  for d in $(find "$1" -type d -and \( -name 'Headers' -and -not -iwholename '*Python*' \)); do
+    if [[ -d "$d" ]]; then
+      echo "$(printf $(basename $(echo "$d" | rev | cut -f 1 | awk -F'krowemarf.' '{ print $2 }' | rev)))"
+      command ln -fs "$d" "$DST_DIR/$(printf $(basename $(echo "$d" | rev | cut -f 1 | awk -F'krowemarf.' '{ print $2 }' | rev)))"
+    fi
+  done
+}
 
-  if [[ -d "$d/ApplicationServices.framework/Versions/A/Frameworks" ]]; then
-    for dd in $(find "$d/ApplicationServices.framework/Versions/A/Frameworks" -maxdepth 4 -type d -name 'Headers'); do
-      if [[ ! -d "$DST_DIR/$(basename $(echo $dd | awk -F. '{print $5}'))" ]] && [[ -d "$dd" ]]; then
-        command ln -fs "$dd" "$DST_DIR/$(basename $(echo $dd | awk -F. '{print $5}'))"
-      fi
-    done
-  fi
+find_framework_header $XCODE_PATH/Contents/Developer/Library/Frameworks
+find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks/
+find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/DriverKit19.0.sdk/System/DriverKit/System/Library/Frameworks
+find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/Library/Apple/System/Library/Frameworks
+find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks
+find_framework_header $XCODE_PATH/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks
 
-  if [[ -d "$d/CoreServices.framework/Versions/A/Frameworks" ]]; then
-    for dd in $(find "$d/CoreServices.framework/Versions/A/Frameworks" -maxdepth 4 -type d -name 'Headers'); do
-      if [[ ! -d "$DST_DIR/$(basename $(echo $dd | awk -F. '{print $5}'))" ]] && [[ -d "$dd" ]]; then
-        command ln -fs "$dd" "$DST_DIR/$(basename $(echo $dd | awk -F. '{print $5}'))"
-      fi
-    done
-  fi
-
-  if [[ -d "$d/Carbon.framework/Versions/A/Frameworks" ]]; then
-    for dd in $(find "$d/ApplicationServicesCarbon.framework/Versions/A/Frameworks" -maxdepth 4 -type d -name 'Headers'); do
-      if [[ ! -d "$DST_DIR/$(basename $(echo $dd | awk -F. '{print $5}'))" ]] && [[ -d "$dd" ]]; then
-        command ln -fs "$dd" "$DST_DIR/$(basename $(echo $dd | awk -F. '{print $5}'))"
-      fi
-    done
-  fi
-done
-
-for d in $(find "$XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/PrivateFrameworks" -maxdepth 1 -type d -or -type l); do
-  if [[ -d "$d/Headers" ]]; then
-    command ln -fs "$d/Headers" "$DST_DIR/$(printf $(basename $d) | awk -F. '{print $1}')"
-  fi
-done
+find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/PrivateFrameworks
+find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/PrivateFrameworks
+find_framework_header $XCODE_PATH/Contents/Developer/Library/PrivateFrameworks
+find_framework_header $XCODE_PATH/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/Developer/Platforms/MacOSX.platform/Developer/Library/PrivateFrameworks

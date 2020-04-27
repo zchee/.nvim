3a6ec1fc21ceba5ac50538a97c084085bb509789
Koichi Shiraishi <zchee.io@gmail.com>
2020-04-28 01:36:25 +0900

    path/Frameworks: fix symlink.bash script

--- path/Frameworks/symlink.bash
+++ path/Frameworks/symlink.bash
@@ -18,20 +18,23 @@ mkdir -p "$DST_DIR"
 find_framework_header() {
   for d in $(find "$1" -type d -and \( -name 'Headers' -and -not -iwholename '*Python*' \)); do
     if [[ -d "$d" ]]; then
-      echo "$(printf $(basename $(echo "$d" | rev | cut -f 1 | awk -F'krowemarf.' '{ print $2 }' | rev)))"
-      command ln -fs "$d" "$DST_DIR/$(printf $(basename $(echo "$d" | rev | cut -f 1 | awk -F'krowemarf.' '{ print $2 }' | rev)))"
+      HEADER_NAME="$(printf "%s" $(basename $(echo "$d" | rev | cut -f 1 | awk -F'krowemarf.' '{ print $2 }' | rev)))"
+      if [[ ! -d "$DST_DIR/$HEADER_NAME" ]]; then
+        echo "$HEADER_NAME"
+        command ln -fs "$d" "$DST_DIR/$HEADER_NAME"
+      fi
     fi
   done
 }
 
-find_framework_header $XCODE_PATH/Contents/Developer/Library/Frameworks
-find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks/
-find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/DriverKit19.0.sdk/System/DriverKit/System/Library/Frameworks
-find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/Library/Apple/System/Library/Frameworks
-find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks
-find_framework_header $XCODE_PATH/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks
+find_framework_header "${XCODE_PATH}/Contents/Developer/Library/Frameworks"
+find_framework_header "${XCODE_PATH}/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks/"
+find_framework_header "${XCODE_PATH}/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/DriverKit19.0.sdk/System/DriverKit/System/Library/Frameworks"
+find_framework_header "${XCODE_PATH}/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/Library/Apple/System/Library/Frameworks"
+find_framework_header "${XCODE_PATH}/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks"
+find_framework_header "${XCODE_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks"
 
-find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/PrivateFrameworks
-find_framework_header $XCODE_PATH/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/PrivateFrameworks
-find_framework_header $XCODE_PATH/Contents/Developer/Library/PrivateFrameworks
-find_framework_header $XCODE_PATH/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/Developer/Platforms/MacOSX.platform/Developer/Library/PrivateFrameworks
+find_framework_header "${XCODE_PATH}/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/PrivateFrameworks"
+find_framework_header "${XCODE_PATH}/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/PrivateFrameworks"
+find_framework_header "${XCODE_PATH}/Contents/Developer/Library/PrivateFrameworks"
+find_framework_header "${XCODE_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/Developer/Platforms/MacOSX.platform/Developer/Library/PrivateFrameworks"

490630d1197e871a869083505eb949c7aeb636d0
Koichi Shiraishi <zchee.io@gmail.com>
2021-04-06 00:46:57 +0900

    lua: update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,34 +1,115 @@
+require('telescope').setup{
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '-i',
+      '--no-config',
+      '--no-ignore-vcs',
+      '--threads=20',
+      '--mmap',
+      '--hidden',
+      '--no-heading',
+      '--vimgrep',
+      '--glob=!.git',
+      '--glob=!node_modules',
+      '--color=never',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case'
+    },
+    prompt_position = "bottom",
+    prompt_prefix = "> ",
+    selection_caret = "> ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_defaults = {
+      horizontal = {
+        mirror = false,
+      },
+      vertical = {
+        mirror = false,
+      },
+      preview_width = 0.5,
+    },
+    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    file_ignore_patterns = {},
+    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    shorten_path = true,
+    winblend = 0,
+    width = 0.5,
+    preview_cutoff = 120,
+    results_height = 1,
+    results_width = 0.3,
+    border = {},
+    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    color_devicons = true,
+    use_less = true,
+    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+
+    -- Developer configurations: Not meant for general override
+    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+  }
+}
+
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
     "bash",
     "c",
+    -- "c_sharp",
+    -- "clojure",
     "cpp",
+    "css",
+    -- "dart",
+    -- "elm",
+    -- "erlang",
+    -- "fennel",
+    -- "gdscript",
     "go",
     "graphql",
+    -- "haskell",
     "html",
     "java",
     "javascript",
     "jsdoc",
     "json",
+    -- "julia",
     -- "kotlin",
+    -- "ledger",
     "lua",
+    -- "nix",
     -- "ocaml",
     -- "ocaml_interface",
     -- "ocamllex",
+    -- "php",
     "python",
+    -- "ql",
     "query",
     "regex",
-    "rst",
+    -- "rst",
     "ruby",
     "rust",
+    -- "scala",
+    -- "sparql",
     "swift",
+    -- "teal",
     "toml",
-    "typescript"
+    "tsx",
+    -- "turtle",
+    "typescript",
+    -- "verilog",
+    -- "vue",
     -- "yaml"
   };
   highlight = {
     enable = true,
-    disable = { 'bash', 'go' }
+    disable = { 'bash', 'go' }  -- , 'typescript'
   };
   incremental_selection = {
     enable = true,
@@ -42,7 +123,7 @@ require'nvim-treesitter.configs'.setup {
   };
   indent = {
     enable = true,
-    disable = { 'go', 'python' }
+    disable = { "json", 'go' }  -- , 'go'
   };
   refactor = {
     highlight_definitions = {
@@ -66,7 +147,7 @@ require'nvim-treesitter.configs'.setup {
     }
   };
   textobjects = {
-    enable = true,
+    enable = false,
     select = {
       enable = true,
       keymaps = {
@@ -87,7 +168,7 @@ require'nvim-treesitter.configs'.setup {
       },
     },
     swap = {
-      enable = true,
+      enable = false,
       swap_next = {
         ["<leader>al"] = "@parameter.inner",
       },
@@ -96,7 +177,7 @@ require'nvim-treesitter.configs'.setup {
       },
     },
     move = {
-      enable = true,
+      enable = false,
       goto_next_start = {
         ["]m"] = "@function.outer",
         ["]]"] = "@class.outer",
@@ -123,7 +204,7 @@ require'nvim-treesitter.configs'.setup {
   };
   rainbow = {
     enable = true,
-    disable = { 'bash', 'go' }
+    disable = { 'bash', 'defx', 'go' }  -- , 'go'
   };
 }
 

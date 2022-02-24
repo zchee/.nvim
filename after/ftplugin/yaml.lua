382dd090e9883d4a9c33b5ad4e26d1a9dabd5851
Koichi Shiraishi <zchee.io@gmail.com>
2022-02-24 16:17:23 +0900

    after/ftplugin: convert yaml.vim to yaml.lua
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ after/ftplugin/yaml.lua
@@ -0,0 +1,7 @@
+vim.opt_local.autoindent = true
+vim.opt_local.expandtab = true
+vim.opt_local.indentkeys:remove { "<:>" }
+vim.opt_local.iskeyword:append { "-", "$", "#" }
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.tabstop = 2

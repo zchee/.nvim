a6e740c5df52320fd7fa5aaef6ddb79d51f34ac9
Koichi Shiraishi <zchee.io@gmail.com>
2023-08-04 13:21:33 +0900

    path: fix symlink.bash script to more better
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- path/symlink.bash
+++ path/symlink.bash
@@ -2,14 +2,47 @@
 set -eo pipefail
 
 warn() { printf "\x1b[1;33m[WARN]\x1b[0m %s\\n" "$1" >&2; }
+error() { printf "\\x1b[1;31m[ERROR]\\x1b[0m %s\\n" "$1" >&2; exit 1; }
 
-XCODE_PATH=$1
+xcode_path="$(xcode-select --print-path)"
+if [ $# == 1 ]; then
+  xcode_path="$1"
+  info "Xcode path: $xcode_path"
+fi
+readonly xcode_path
 
-if [[ -z $XCODE_PATH ]]; then
-  echo "USAGE: $(basename "$0") [/Applications/Xcode.app | /Applications/Xcode-beta.app | \$(xcode-select --print-path)]"; exit 1
+if [[ ! -d $xcode_path ]]; then
+  error "
+Usage:
+  $(basename "$0") [/Applications/Xcode.app | /Applications/Xcode-beta.app | \$(xcode-select --print-path)]"
 fi
 
 DST_DIR='./Frameworks'
+if [[ ! -d "$DST_DIR" ]]; then
+  mkdir -p "$DST_DIR"
+fi
+
+clean_symlink() {
+  for d in $(find ./Frameworks -type l);
+  do
+    unlink $d
+  done
+}
+
+find_framework_header() {
+  for d in $(fd -j $(nproc) -t d -t l 'Headers$' --exclude='AppleTVOS.platform' --exclude='AppleTVSimulator.platform' --exclude='WatchOS.platform' --exclude='WatchSimulator.platform' --exclude='iPhoneOS.platform' --exclude='iPhoneSimulator.platform' --exclude='XROS.platform' --exclude='XRSimulator.platform' --exclude='iOSSupport' --exclude='Python.framework' --exclude='Python3.framework' --exclude='Colloqui' "$1"); do
+    HEADER_NAME="$(printf "%s" $(basename $(echo "$d" | rev | cut -f 1 | awk -F'krowemarf.' '{ print $2 }' | rev)))"
+    if [[ -d "$DST_DIR/$HEADER_NAME" ]]; then
+      continue
+    fi
+    echo "$HEADER_NAME"
+    command ln -fs "$d" "$DST_DIR/$HEADER_NAME"
+  done
+}
+
+clean_symlink
+find_framework_header "$xcode_path"
+
 # DST_DIR="${2:-$(basename "${XCODE_PATH%.*}")}"
 # if [[ "$DST_DIR" != 'Xcode' ]]; then
 #   DST_DIR="$(basename "$XCODE_PATH")"
@@ -44,16 +77,3 @@ DST_DIR='./Frameworks'
 # find_framework_header "${XCODE_PATH}/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/PrivateFrameworks"
 # find_framework_header "${XCODE_PATH}/Contents/Developer/Library/PrivateFrameworks"
 # find_framework_header "${XCODE_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/Developer/Platforms/MacOSX.platform/Developer/Library/PrivateFrameworks"
-
-find_framework_header() {
-  for d in $(fd -j $(nproc) -t d -t l 'Headers$' --exclude='AppleTVOS.platform' --exclude='AppleTVSimulator.platform' --exclude='WatchOS.platform' --exclude='WatchSimulator.platform' --exclude='iPhoneOS.platform' --exclude='iPhoneSimulator.platform' --exclude='iOSSupport' --exclude='Python.framework' --exclude='Python3.framework' --exclude='Colloqui' "$1"); do
-    HEADER_NAME="$(printf "%s" $(basename $(echo "$d" | rev | cut -f 1 | awk -F'krowemarf.' '{ print $2 }' | rev)))"
-    if [[ -d "$DST_DIR/$HEADER_NAME" ]]; then
-      continue
-    fi
-    echo "$HEADER_NAME"
-    command ln -fs "$d" "$DST_DIR/$HEADER_NAME"
-  done
-}
-
-find_framework_header "$XCODE_PATH"

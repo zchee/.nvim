5380c5e5ae517735290a488ebcf6084e9cd43176
Koichi Shiraishi <zchee.io@gmail.com>
2023-08-05 07:37:39 +0900

    syntax/gohtmltmpl: fix to use native nvim lua api
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- syntax/gohtmltmpl.lua
+++ syntax/gohtmltmpl.lua
@@ -1,21 +1,20 @@
--- This file copied from https://github.com/fatih/vim-go/blob/4a429a0fc85c/syntax/gohtmltmpl.vim
+if vim.b.current_syntax then
+  return
+end
+
+if not vim.g.main_syntax then
+  vim.g.main_syntax = "html"
+end
 
 vim.cmd([[
-if exists("b:current_syntax")
-  finish
-endif
-
-if !exists("g:main_syntax")
-  let g:main_syntax = 'html'
-endif
-
-runtime! syntax/gotexttmpl.lua
-runtime! syntax/html.vim
-unlet b:current_syntax
-
-syn cluster htmlPreproc add=gotplAction,goTplComment
-
-let b:current_syntax = "gohtmltmpl"
+  runtime! syntax/gotexttmpl.lua
+  runtime! syntax/html.lua
 ]])
 
--- vim: sw=2 ts=2 et
+vim.b.current_syntax = nil
+
+vim.cmd([[
+  syn cluster htmlPreproc add=gotplAction,goTplComment
+]])
+
+vim.b.current_syntax = "gohtmltmpl"

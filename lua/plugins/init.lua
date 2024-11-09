542cefded92fd4539cbb6e797cad7ba35f22c3cb
Koichi Shiraishi <zchee.io@gmail.com>
2024-11-09 21:54:55 +0900

    lua/plugins/init: add LiadOz/nvim-dap-repl-highlights & PERF treesitter
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -188,6 +188,7 @@ return {
     dependencies = {
       "rcarriga/nvim-dap-ui",
       "theHamsta/nvim-dap-virtual-text",
+      "LiadOz/nvim-dap-repl-highlights",
       "leoluz/nvim-dap-go",
       "jbyuki/one-small-step-for-vimkind",
       "nvim-neotest/nvim-nio",
@@ -219,8 +220,18 @@ return {
       "rush-rs/tree-sitter-asm",
       "yamatsum/nvim-nonicons",
     },
-    lazy = false,
-    run = "TSUpdate",
+    event = { "VeryLazy" },
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+      -- Luckily, the only things that those plugins need are the custom queries, which we make available
+      -- during startup.
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require("nvim-treesitter.query_predicates")
+    end,
+    build = ":TSUpdate",
     config = function()
       require("plugins.treesitter")
     end,

d59859f22d0fa234230614b31e74a0b73f96db5d
Koichi Shiraishi <zchee.io@gmail.com>
2025-01-18 21:53:03 +0900

    lua/plugins/lsp/pyright: update pyright config
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- lua/plugins/lsp/pyright.lua
+++ lua/plugins/lsp/pyright.lua
@@ -1,31 +1,33 @@
+local util = require("util")
+
 --- @class lspconfig.Config : vim.lsp.ClientConfig
 return {
-	settings = {
-		python = {
-			disableLanguageServices = false,
-			disableOrganizeImports = false,
-			openFilesOnly = false,
-			analysis = {
-				autoImportCompletions = true,
-				autoSearchPaths = true,
-				diagnosticMode = "workspace", -- "openFilesOnly"
-				diagnosticSeverityOverrides = {},
-				exclude = {},
-				extraPaths = {
-					-- "/usr/local/google-cloud-sdk/lib",
-					-- "/usr/local/google-cloud-sdk/lib/third_party",
-					vim.fn.getcwd() .. "/third_party",
-				},
-				ignore = {},
-				include = {},
-				logLevel = "Error",
-				stubPath = "",
-				typeCheckingMode = "off", -- "basic", -- "strict"
-				typeshedPaths = {},
-			},
-			useLibraryCodeForTypes = true,
-			functionSignatureDisplay = "formatted",
-			venvPath = ".venv",
-		},
-	},
+  cmd = { util.homebrew_binary("pyright", "pyright-langserver"), "--stdio" },
+  settings = {
+    python = {
+      disableLanguageServices = false,
+      disableOrganizeImports = false,
+      openFilesOnly = false,
+      analysis = {
+        autoImportCompletions = true,
+        autoSearchPaths = true,
+        diagnosticMode = "workspace", -- "openFilesOnly"
+        diagnosticSeverityOverrides = {},
+        exclude = {},
+        extraPaths = {
+          -- "/usr/local/google-cloud-sdk/lib",
+          -- "/usr/local/google-cloud-sdk/lib/third_party",
+          vim.fn.getcwd() .. "/third_party",
+        },
+        ignore = {},
+        include = {},
+        logLevel = "Error",
+        stubPath = "",
+        typeCheckingMode = "off", -- "off", "basic", "standard", "strict",
+        typeshedPaths = {},
+      },
+      useLibraryCodeForTypes = true,
+      functionSignatureDisplay = "formatted",
+    },
+  },
 }

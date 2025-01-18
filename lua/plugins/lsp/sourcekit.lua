e00979784850219a438363778b535895062af3eb
Koichi Shiraishi <zchee.io@gmail.com>
2025-01-18 21:53:29 +0900

    lua/plugins/lsp/sourcekit: update sourcekit config
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- lua/plugins/lsp/sourcekit.lua
+++ lua/plugins/lsp/sourcekit.lua
@@ -1,20 +1,20 @@
 local lspconfig = require("lspconfig")
+local lspconfig_util = require("lspconfig.util")
 
 --- @class lspconfig.Config : vim.lsp.ClientConfig
 return {
-  autostart = false,
-  cmd = {
-    "sourcekit-lsp",
-    "-c", "release",
-    "--completion-server-side-filtering=false",
-    "--completion-max-results", "500",
-  },
-  filetypes = { "swift", "objective-c", "objective-cpp" }, -- , "c", "cpp"
+  autostart = true,
+  cmd = { "/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin/sourcekit-lsp", "--configuration=release", "--experimental-feature=example-case" },
+  filetypes = { "swift", "objective-c", "objective-cpp" },
   root_dir = function(filename, _)
-    -- better to keep it at the end, because some modularized apps contain multiple Package.swift files
-    return lspconfig.util.root_pattern "buildServer.json" (filename)
-        or lspconfig.util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
-        or lspconfig.util.root_pattern("compile_commands.json", "Package.swift")(filename)
-        or lspconfig.util.find_git_ancestor(filename)
+    return lspconfig_util.root_pattern "buildServer.json" (filename)
+        or lspconfig_util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+        -- better to keep it at the end, because some modularized apps contain multiple Package.swift files
+        or lspconfig_util.root_pattern("compile_commands.json", "Package.swift")(filename)
+        or lspconfig_util.find_git_ancestor(filename)
+  end,
+  get_language_id = function(_, ftype)
+    local t = { objc = "objective-c", objcpp = "objective-cpp" }
+    return t[ftype] or ftype
   end,
 }

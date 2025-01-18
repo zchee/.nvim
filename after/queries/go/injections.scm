dac2a646ed461d94e902dfced5d2a063d17063a8
Koichi Shiraishi <zchee.io@gmail.com>
2025-01-18 21:47:12 +0900

    after/queries/go: fix ts injections
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- after/queries/go/injections.scm
+++ after/queries/go/injections.scm
@@ -1,32 +1,32 @@
 ; Code token from: https://github.com/ray-x/go.nvim/blob/master/after/queries/go/injections.scm
 
-; extends
+;; extends
 
 ; inject sql in single line strings
 ; e.g. db.GetContext(ctx, "SELECT * FROM users WHERE name = 'John'")
-
-((call_expression
-  (selector_expression
-    field: (field_identifier) @_field)
-  (argument_list
-    (interpreted_string_literal) @sql))
-  (#any-of? @_field "Exec" "GetContext" "ExecContext" "SelectContext" "In"
-				            "RebindNamed" "Rebind" "Query" "QueryRow" "QueryRowxContext" "NamedExec" "MustExec" "Get" "Queryx")
-  (#offset! @sql 0 1 0 -1))
-
-; still buggy for nvim 0.10
-((call_expression
-  (selector_expression
-    field: (field_identifier) @_field (#any-of? @_field "Exec" "GetContext" "ExecContext" "SelectContext" "In" "RebindNamed" "Rebind" "Query" "QueryRow" "QueryRowxContext" "NamedExec" "MustExec" "Get" "Queryx"))
-  (argument_list
-    (interpreted_string_literal) @injection.content))
-  (#offset! @injection.content 0 1 0 -1)
-  (#set! injection.language "sql"))
+; following no longer works after https://github.com/tree-sitter/tree-sitter-go/commit/47e8b1fae7541f6e01cead97201be19321ec362a
+; ((call_expression
+;   (selector_expression
+;     field: (field_identifier) @_field)
+;   (argument_list
+;     (interpreted_string_literal) @sql))
+;   (#any-of? @_field "Exec" "GetContext" "ExecContext" "SelectContext" "In"
+; 				            "RebindNamed" "Rebind" "Query" "QueryRow" "QueryRowxContext" "NamedExec" "MustExec" "Get" "Queryx")
+;   (#offset! @sql 0 1 0 -1))
+;
+; ; still buggy for nvim 0.10
+; ((call_expression
+;   (selector_expression
+;     field: (field_identifier) @_field (#any-of? @_field "Exec" "GetContext" "ExecContext" "SelectContext" "In" "RebindNamed" "Rebind" "Query" "QueryRow" "QueryRowxContext" "NamedExec" "MustExec" "Get" "Queryx"))
+;   (argument_list
+;     (interpreted_string_literal) @injection.content))
+;   (#offset! @injection.content 0 1 0 -1)
+;   (#set! injection.language "sql"))
 
 ; neovim nightly 0.10
 ([
-  (interpreted_string_literal)
-  (raw_string_literal)
+  (interpreted_string_literal_content)
+  (raw_string_literal_content)
   ] @injection.content
  (#match? @injection.content "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
  (#offset! @injection.content 0 1 0 -1)
@@ -34,8 +34,8 @@
 
 ; a general query injection
 ([
-   (interpreted_string_literal)
-   (raw_string_literal)
+   (interpreted_string_literal_content)
+   (raw_string_literal_content)
  ] @sql
  (#match? @sql "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
  (#offset! @sql 0 1 0 -1))
@@ -44,8 +44,8 @@
 ; fallback keyword and comment based injection
 
 ([
-  (interpreted_string_literal)
-  (raw_string_literal)
+  (interpreted_string_literal_content)
+  (raw_string_literal_content)
  ] @sql
  (#contains? @sql "-- sql" "--sql" "ADD CONSTRAINT" "ALTER TABLE" "ALTER COLUMN"
                   "DATABASE" "FOREIGN KEY" "GROUP BY" "HAVING" "CREATE INDEX" "INSERT INTO"
@@ -55,8 +55,8 @@
 
 ; nvim 0.10
 ([
-  (interpreted_string_literal)
-  (raw_string_literal)
+  (interpreted_string_literal_content)
+  (raw_string_literal_content)
  ] @injection.content
  (#contains? @injection.content "-- sql" "--sql" "ADD CONSTRAINT" "ALTER TABLE" "ALTER COLUMN"
                   "DATABASE" "FOREIGN KEY" "GROUP BY" "HAVING" "CREATE INDEX" "INSERT INTO"

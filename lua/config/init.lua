4ab37eb07c7f183d1a00a7ce4d6f867c5c5e656b
Koichi Shiraishi <zchee.io@gmail.com>
2025-01-26 02:16:39 +0900

    lua/config/init: add require("plugins/plenary")
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,5 @@
+require("plugins/plenary")
+
 require("config.nvim")
 
 require("config.autocmd")
@@ -5,5 +7,3 @@ require("config.command")
 require("config.functions")
 require("config.highlight")
 require("config.keymap")
-
-require("config.which_key")

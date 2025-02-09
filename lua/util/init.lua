1c0f56a99c2845fbdfa9ebe40f1083423ccadd53
Koichi Shiraishi <zchee.io@gmail.com>
2025-02-10 06:33:21 +0900

    lua/util: add switch function and improve docs
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -1,11 +1,62 @@
 local M = {}
 
+---Switch returns function instead of table
+---http://lua-users.org/wiki/SwitchStatement
+---
+---Usage:
+--- for case = 1,4 do
+---   switch(case) {
+---     [1] = function() print("one") end,
+---     [2] = print,
+---     default = function(x) print("default",x) end,
+---   }
+--- end
+---
+---@param case any
+---@return function
+function M.switch(case)
+  return function(codetbl)
+    local f = codetbl[case] or codetbl.default
+    if f then
+      if type(f) == "function" then
+        return f(case)
+      else
+        error("case " .. tostring(case) .. " not a function")
+      end
+    end
+  end
+end
+
+----@param condition any
+----@return function
+-- function M.switch(condition)
+--   return {
+--     var = condition,
+--
+--     of = function(self, code)
+--       local f
+--       if (self.var) then
+--         f = code[self.var] or code.default
+--       else
+--         f = code.default
+--       end
+--       if f then
+--         if type(f) == "function" then
+--           return f(self.var, self)
+--         else
+--           error("case " .. tostring(self.var) .. " not a function")
+--         end
+--       end
+--     end
+--   }
+-- end
+
 ---
 ---Return XDG_CACHE_HOME
 ---
 ---@return string
 function M.xdg_cache_home()
-  return tostring(os.getenv("XDG_CACHE_HOME"))
+  return M.readlink(tostring(os.getenv("XDG_CACHE_HOME")))
 end
 
 ---
@@ -13,7 +64,7 @@ end
 ---
 ---@return string
 function M.xdg_config_home()
-  return tostring(os.getenv("XDG_CONFIG_HOME"))
+  return M.readlink(tostring(os.getenv("XDG_CONFIG_HOME")))
 end
 
 ---
@@ -21,7 +72,7 @@ end
 ---
 ---@return string
 function M.xdg_data_home()
-  return tostring(os.getenv("XDG_DATA_HOME"))
+  return M.readlink(tostring(os.getenv("XDG_DATA_HOME")))
 end
 
 ---
@@ -29,22 +80,22 @@ end
 ---
 ---@return string
 function M.xdg_state_home()
-  return tostring(os.getenv("XDG_STATE_HOME"))
+  return M.readlink(tostring(os.getenv("XDG_STATE_HOME")))
 end
 
----@param ...string
+---@param ... string
 ---@return string
 function M.go_path(...)
   return vim.fs.joinpath(vim.uv.os_homedir(), "go", ...)
 end
 
----@param ...string
+---@param ... string
 ---@return string
 function M.src_path(...)
   return vim.fs.joinpath(vim.uv.os_homedir(), "src", ...)
 end
 
----@param ...string
+---@param ... string
 ---@return string
 function M.src_path(...)
   return vim.fs.joinpath(vim.uv.os_homedir(), "src", ...)
@@ -53,12 +104,9 @@ end
 ---
 ---expands env path and reads symbolic link.
 ---
----@param path string? | nil
+---@param path string
 ---@return string
 function M.readlink(path)
-  if path == nil then
-    return ""
-  end
   return vim.uv.fs_readlink(path) or ""
 end
 

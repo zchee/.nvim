53248bdf8421a84324901d0c8bfc4ef1d381d7b9
Koichi Shiraishi <zchee.io@gmail.com>
2025-11-25 20:20:43 +0900

    lua/lsp/jsonls: migrate to bun and add custom schemas

--- lua/lsp/jsonls.lua
+++ lua/lsp/jsonls.lua
@@ -2,26 +2,37 @@ local util = require "util"
 
 --- @class vim.lsp.Config : vim.lsp.ClientConfig
 return {
-  cmd = { util.src_path("github.com/hrsh7th/vscode-langservers-extracted/bin/vscode-json-language-server"), "--stdio" },
+  -- cmd = { util.src_path("github.com/hrsh7th/vscode-langservers-extracted/bin/vscode-json-language-server"), "--stdio" },
+  cmd = { util.bun_prefix("vscode-json-language-server"), "--stdio" },
   filetypes = { "json", "json5", "jsonschema" },
   -- https://github.com/microsoft/vscode/blob/main/extensions/json-language-features/package.json
   settings = {
     json = {
-      schemas = require("schemastore").json.schemas(),
-      -- schemas = {
-      --   -- {
-      --   --   url = "/Users/zchee/src/github.com/zchee/schema/claude-code.schema.json",
-      --   --   fileMatch = ".*/%.claude.json",
-      --   -- },
-      --   -- {
-      --   --   url = "/Users/zchee/src/github.com/zchee/schema/claude-code.settings.schema.json",
-      --   --   fileMatch = ".*/%.claude/settings.json",
-      --   -- },
-      --   {
-      --     url = "https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/tsconfig.json",
-      --     fileMatch = ".*/%.tsconfig.json",
-      --   },
-      -- },
+      schemas = vim.tbl_deep_extend("force", require("schemastore").json.schemas(), {
+        {
+          url = "https://raw.githubusercontent.com/google-gemini/gemini-cli/main/schemas/settings.schema.json",
+          fileMatch = ".*/\\.?gemini/settings.json",
+        },
+        {
+          url = "/Users/zchee/src/github.com/zchee/schema/claude-code.schema.json",
+          -- url = "https://json.schemastore.org/claude-code-settings.json",
+          -- url = "https://raw.githubusercontent.com/zchee/schema/refs/heads/main/claude-code.schema.json",
+          -- fileMatch = ".*/\\.claude.json",
+          fileMatch = ".*/%.claude.json",
+        },
+        -- {
+        --   url = "/Users/zchee/src/github.com/zchee/schema/claude-code.schema.json",
+        --   fileMatch = ".*/%.claude.json",
+        -- },
+        -- {
+        --   url = "/Users/zchee/src/github.com/zchee/schema/claude-code.settings.schema.json",
+        --   fileMatch = ".*/%.claude/settings.json",
+        -- },
+        {
+          url = "https://json.schemastore.org/tsconfig.json",
+          fileMatch = ".*/%.tsconfig.json",
+        },
+      }),
       validate = {
         enable = true,
       },

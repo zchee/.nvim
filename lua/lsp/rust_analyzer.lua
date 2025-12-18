812f3db2feb87c36ba7d64225069230f5c0f39af
Koichi Shiraishi <zchee.io@gmail.com>
2025-12-19 03:54:48 +0900

    lua/lsp: cleanup rust_analyzer config
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- lua/lsp/rust_analyzer.lua
+++ lua/lsp/rust_analyzer.lua
@@ -1,76 +1,15 @@
+--- https://github.com/rust-lang/rust-analyzer/blob/master/crates/rust-analyzer/src/config.rs
+--- https://rust-analyzer.github.io/book/configuration.html
+
 --- @class vim.lsp.Config : vim.lsp.ClientConfig
 return {
   cmd = { "rust-analyzer" },
   root_markers = { "Cargo.toml", ".git" },
-  -- init_options = {
-  --   server = {
-  --     extraEnv = {
-  --       ["RUSTUP_TOOLCHAIN"] = "nightly",
-  --       ["RUSTFLAGS"] = os.getenv("RUSTFLAGS"),
-  --       ["CHALK_OVERFLOW_DEPTH"] = "100000000",
-  --       ["CHALK_SOLVER_MAX_SIZE"] = "100000000",
-  --     },
-  --   },
-  --   ["rust-analyzer"] = {
-  --     completion = {
-  --       fullFunctionSignatures = {
-  --         enable = true,
-  --       },
-  --       hideDeprecated = true,
-  --     },
-  --     -- diagnostics = {
-  --     --   experimental = "enable",
-  --     -- },
-  --     lens = {
-  --       debug = { enable = true },
-  --       enable = true,
-  --       implementations = { enable = true },
-  --       references = {
-  --         adt = { enable = true },
-  --         enumVariant = { enable = true },
-  --         method = { enable = true },
-  --         trait = { enable = true },
-  --       },
-  --       run = { enable = true },
-  --       updateTest = { enable = true },
-  --     },
-  --     inlayHints = {
-  --       bindingModeHints = {
-  --         enable = true,
-  --       },
-  --       closureCaptureHints = {
-  --         enable = true,
-  --       },
-  --       genericParameterHints = {
-  --         lifetime = {
-  --           enable = true,
-  --         },
-  --         type = {
-  --           enable = true,
-  --         },
-  --       },
-  --       implicitSizedBoundHints = {
-  --         enable = true,
-  --       },
-  --       lifetimeElisionHints = {
-  --         useParameterNames = true,
-  --       },
-  --     },
-  --   }
-  -- },
   settings = {
-    -- server = {
-    --   extraEnv = {
-    --     ["RUSTUP_TOOLCHAIN"] = "nightly",
-    --     ["RUSTFLAGS"] = os.getenv("RUSTFLAGS"),
-    --     ["CHALK_OVERFLOW_DEPTH"] = "100000000",
-    --     ["CHALK_SOLVER_MAX_SIZE"] = "100000000",
-    --   },
-    -- },
     ["rust-analyzer"] = {
       cachePriming = {
         enabled = true,
-        numThreads = 0,
+        numThreads = "physical",
       },
       completion = {
         fullFunctionSignatures = {
@@ -108,72 +47,4 @@ return {
       },
     },
   },
-  --   cachePriming = {
-  --     numThreads = "physical",
-  --   },
-  --   completion = {
-  --     privateEditable = {
-  --       enable = true,
-  --     },
-  --   },
-  --   diagnostics = {
-  --     experimental = {
-  --       enable = true,
-  --     },
-  --   },
-  --   files = {
-  --     watcher = "server",
-  --   },
-  --   hover = {
-  --     actions = {
-  --       references = {
-  --         enable = true,
-  --       },
-  --     },
-  --     memoryLayout = {
-  --       niches = true,
-  --       offset = "both",
-  --       padding = "both",
-  --     },
-  --   },
-  --   numThreads = "physical",
-  --   workspace = {
-  --     symbol = {
-  --       search = {
-  --         kind = "all_symbols",
-  --         scope = "workspace_and_dependencies",
-  --       },
-  --     },
-  --   },
-  -- },
-
-  -- rust = {
-  --   analyzerTargetDir = "target/rust-analyzer",
-  --   extraEnv = {
-  --     ["RUSTFLAGS"] = os.getenv("RUSTFLAGS"),
-  --   },
-  -- },
-  -- cargo = {
-  --   features = "all",
-  --   extraEnv = {
-  --     ["RUSTFLAGS"] = os.getenv("RUSTFLAGS"),
-  --     ["SKIP_WASM_BUILD"] = "1",
-  --   },
-  --   loadOutDirsFromCheck = true,
-  --   buildScripts = {
-  --     enable = true,
-  --   },
-  -- },
-  -- rustfmt = {
-  --   -- extraArgs = { "+nightly-2023-11-01" },
-  -- },
-  -- -- imports = {
-  -- --   granularity = {
-  -- --     group = "module",
-  -- --   },
-  -- --   prefix = "self",
-  -- -- },
-  -- procMacro = {
-  --   enable = true,
-  -- },
 }
